7c26496180d5ca8ad332eff6541102526cc5eca0
Angzhao <wuangzhao@gmail.com>
2018-04-08 18:00:35 +0800

    Github

--- Chapter_02/stereo-javaconfig/src/main/java/soundsystem/CDPlayerConfig.java
+++ Chapter_02/stereo-javaconfig/src/main/java/soundsystem/CDPlayerConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CDPlayerConfig {
   
-  @Bean
+  @Bean//告诉Spring这个方法将会返回一个对象，该对象要注册为Spring应用上下文中的bean。
   public CompactDisc compactDisc() {
     return new SgtPeppers();
   }
