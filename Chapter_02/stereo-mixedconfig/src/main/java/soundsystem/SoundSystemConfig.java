39e10fba6b1816c625c30aeea83a4411ef336068
Angzhao <wuangzhao@gmail.com>
2018-04-08 21:00:53 +0800

    Github

--- Chapter_02/stereo-mixedconfig/src/main/java/soundsystem/SoundSystemConfig.java
+++ Chapter_02/stereo-mixedconfig/src/main/java/soundsystem/SoundSystemConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@Import(CDPlayerConfig.class)
-@ImportResource("classpath:cd-config.xml")
+@Import(CDPlayerConfig.class)//导入
+@ImportResource("classpath:cd-config.xml")//同时让Spring加载它和其他基于Java的配置
 public class SoundSystemConfig {
 
 }
