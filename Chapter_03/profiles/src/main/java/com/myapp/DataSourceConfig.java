7c7bf7ce3d67a09f92af9c777bc45b074c69b3d5
Angzhao <wuangzhao@gmail.com>
2018-04-10 22:34:06 +0800

    Github

--- Chapter_03/profiles/src/main/java/com/myapp/DataSourceConfig.java
+++ Chapter_03/profiles/src/main/java/com/myapp/DataSourceConfig.java
@@ -11,9 +11,10 @@ import org.springframework.jndi.JndiObjectFactoryBean;
 
 @Configuration
 public class DataSourceConfig {
-  
+  //只有规定的profile激活时，相应的bean才会被创建，但是可能会有其他的bean并没有声明在一个给定的profile范围内。
+  // 没有指定的profile的bean始终都会被创建，与激活哪个profile没有关系
   @Bean(destroyMethod = "shutdown")
-  @Profile("dev")
+  @Profile("dev")//为dev profile 装配的bean
   public DataSource embeddedDataSource() {
     return new EmbeddedDatabaseBuilder()
         .setType(EmbeddedDatabaseType.H2)
@@ -23,7 +24,7 @@ public class DataSourceConfig {
   }
 
   @Bean
-  @Profile("prod")
+  @Profile("prod")//为prod profile 装配的bean
   public DataSource jndiDataSource() {
     JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
     jndiObjectFactoryBean.setJndiName("jdbc/myDS");
