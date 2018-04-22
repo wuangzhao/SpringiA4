a342dc1797ccaffd560d49dc15e1c54ba0589a52
Angzhao <wuangzhao@gmail.com>
2018-04-22 22:29:47 +0800

    Github

--- Chapter_05/Spittr/src/main/java/spittr/data/SpittleRepository.java
+++ Chapter_05/Spittr/src/main/java/spittr/data/SpittleRepository.java
@@ -8,6 +8,10 @@ public interface SpittleRepository {
 
   List<Spittle> findRecentSpittles();
 
+  /**
+   * max代表返回Spittle ID属性的最大值
+   *count参数声明要返回多少个Spittle对象
+   */
   List<Spittle> findSpittles(long max, int count);
   
   Spittle findOne(long id);
--- Chapter_05/Spittr/src/main/java/spittr/web/HomeController.java
+++ Chapter_05/Spittr/src/main/java/spittr/web/HomeController.java
@@ -6,10 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 可以将其映射到对/homepage的请求
+ * @ RequestMapping（{"/", "/homepage"})
+ * 现在HomeController的home()方法能够映射到对"/"和"/homepage"的GET的请求
+ */
+
 @Controller
 @RequestMapping("/")//将控制器映射到"/"
 public class HomeController {
 
+    //路径现在被转移到类级别@RequestMapping上。
+    //当控制器在类级别上添加@RequestMapping注解时，这个注解会应用到控制器的所有处理器的方法上。
+    //处理器方法上的@RequestMapping注解会类级别上的@RequestMapping的声明进行补充
     @RequestMapping(method = GET)//处理GET请求
     public String home(Model model) {
         return "home";//视图名为home
