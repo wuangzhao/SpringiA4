cb8aafad213bd23679a3cc9c3333708c57804ccc
Angzhao <wuangzhao@gmail.com>
2018-04-22 22:05:48 +0800

    Github

--- Chapter_05/Spittr/src/main/java/spittr/web/HomeController.java
+++ Chapter_05/Spittr/src/main/java/spittr/web/HomeController.java
@@ -7,12 +7,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/")//将控制器映射到"/"
 public class HomeController {
 
-  @RequestMapping(method = GET)
-  public String home(Model model) {
-    return "home";
-  }
+    @RequestMapping(method = GET)//处理GET请求
+    public String home(Model model) {
+        return "home";//视图名为home
+    }
 
 }
--- Chapter_05/Spittr/src/test/java/spittr/web/HomeControllerTest.java
+++ Chapter_05/Spittr/src/test/java/spittr/web/HomeControllerTest.java
@@ -1,4 +1,6 @@
 package spittr.web;
+
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
@@ -6,16 +8,16 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.ui.Model;
 import spittr.web.HomeController;
 
 public class HomeControllerTest {
 
-  @Test
-  public void testHomePage() throws Exception {
-    HomeController controller = new HomeController();
-    MockMvc mockMvc = standaloneSetup(controller).build();
-    mockMvc.perform(get("/"))
-           .andExpect(view().name("home"));
-  }
-
+    @Test
+    public void testHomePage() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = standaloneSetup(controller).build();//搭建MockMvc
+        mockMvc.perform(get("/"))//对"/"执行GET请求
+                .andExpect(view().name("home"));//预期得到home视图
+    }
 }
