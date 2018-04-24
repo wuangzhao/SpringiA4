ea1a8d6021fe5da76118e2d52704f19bcffd08e5
Angzhao <wuangzhao@gmail.com>
2018-04-24 10:44:56 +0800

    Github

--- Chapter_05/Spittr/src/main/java/spittr/web/SpittleController.java
+++ Chapter_05/Spittr/src/main/java/spittr/web/SpittleController.java
@@ -18,35 +18,42 @@ import spittr.data.SpittleRepository;
 @RequestMapping("/spittles")
 public class SpittleController {
 
-  private static final String MAX_LONG_AS_STRING = "9223372036854775807";
-  
-  private SpittleRepository spittleRepository;
+    private static final String MAX_LONG_AS_STRING = "9223372036854775807";
 
-  @Autowired
-  public SpittleController(SpittleRepository spittleRepository) {
-    this.spittleRepository = spittleRepository;
-  }
+    private SpittleRepository spittleRepository;
 
-  @RequestMapping(method=RequestMethod.GET)
-  public List<Spittle> spittles(
-      @RequestParam(value="max", defaultValue=MAX_LONG_AS_STRING) long max,
-      @RequestParam(value="count", defaultValue="20") int count) {
-    return spittleRepository.findSpittles(max, count);
-  }
+    @Autowired
+    public SpittleController(SpittleRepository spittleRepository) {
+        this.spittleRepository = spittleRepository;
+    }
 
-  @RequestMapping(value="/{spittleId}", method=RequestMethod.GET)
-  public String spittle(
-      @PathVariable("spittleId") long spittleId, 
-      Model model) {
-    model.addAttribute(spittleRepository.findOne(spittleId));
-    return "spittle";
-  }
+    /**
+     * @param max
+     * @param count 要同时处理有参数和没有参数的场景，需要对其进行修改，让它们能接受参数
+     *              如果参数在请求中不存在的话，就使用默认值
+     * @return
+     * @RequestParam 用于将请求参数区数据映射到功能处理方法的参数上
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Spittle> spittles(
+            @RequestParam(value = "max", defaultValue = MAX_LONG_AS_STRING) long max,
+            @RequestParam(value = "count", defaultValue = "20") int count) {
+        return spittleRepository.findSpittles(max, count);
+    }
 
-  @RequestMapping(method=RequestMethod.POST)
-  public String saveSpittle(SpittleForm form, Model model) throws Exception {
-    spittleRepository.save(new Spittle(null, form.getMessage(), new Date(), 
-        form.getLongitude(), form.getLatitude()));
-    return "redirect:/spittles";
-  }
+    @RequestMapping(value = "/{spittleId}", method = RequestMethod.GET)
+    public String spittle(
+            @PathVariable("spittleId") long spittleId,
+            Model model) {
+        model.addAttribute(spittleRepository.findOne(spittleId));
+        return "spittle";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String saveSpittle(SpittleForm form, Model model) {
+        spittleRepository.save(new Spittle(null, form.getMessage(), new Date(),
+                form.getLongitude(), form.getLatitude()));
+        return "redirect:/spittles";
+    }
 
 }
