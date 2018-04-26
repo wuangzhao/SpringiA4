a7dbe6e64aa5ff2868b986fc18f917b00c7a8e81
Angzhao <wuangzhao@gmail.com>
2018-04-26 22:44:37 +0800

    Github

--- Chapter_04/lAspect/src/main/java/soundsystem/SingerAspect.java
+++ Chapter_04/lAspect/src/main/java/soundsystem/SingerAspect.java
@@ -1,9 +1,7 @@
 package soundsystem;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,15 +10,25 @@ public class SingerAspect {
     @Pointcut("execution(* soundsystem.Tony.singing(..))")
     public void pointcut(){}
 
-    @Around("pointcut()")
-    public void around(ProceedingJoinPoint proceedingJoinPoint){
-        try {
-            System.out.println("Before");
-            proceedingJoinPoint.proceed();
-            System.out.println("After");
-        } catch (Throwable throwable) {
-            System.out.println(throwable.getMessage());
-        }
+//    @Around("pointcut()")
+//    public void around(ProceedingJoinPoint proceedingJoinPoint){
+//        try {
+//            System.out.println("Before");
+//            proceedingJoinPoint.proceed();
+//            System.out.println("After");
+//        } catch (Throwable throwable) {
+//            System.out.println(throwable.getMessage());
+//        }
+//    }
+
+    @Before("pointcut()")
+    public void before(){
+        System.out.println("***before");
+    }
+
+    @After("pointcut()")
+    public void after(){
+        System.out.println("***after");
     }
 
 }
